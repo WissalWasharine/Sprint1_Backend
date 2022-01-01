48dc7baa4717178da953c76bca137d4623bd516b
WissalWasharine <83767534+WissalWasharine@users.noreply.github.com>
2022-01-01 06:14:15 +0100

    Add files via upload

---
+++ BackendSprint1SOA/src/main/java/com/washarine/vehicules/entities/VehiculeProjection.java
@@ -0,0 +1,11 @@
+package com.washarine.vehicules.entities;
+
+/*import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name = "name", types = { Race.class })
+public interface RaceProjection {
+	
+	public String getName();
+
+}
+*/
